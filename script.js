29530fbdbf049097ae27e2103a9a5f391f8bec94
nejlael <95547043+nejlael@users.noreply.github.com>
2022-11-08 21:03:50 +0100

    Ajout de l'animation
    requestAnimationFrame() à chaque milisecondes pour la transition de l'aiguille des secondes.

--- script.js
+++ script.js
@@ -11,16 +11,15 @@ function clock(){
     const seconds = date.getSeconds();
     const hour = hours * 30; // 360/12=30
     const minute = minutes * 6; // 360/60=6
-    const second = seconds * 6; // " " " "
+    const second = (seconds + date.getMilliseconds()/1000) / 60 * 360 ;
 
 
 // afficher
 
-document.querySelector('.heure').style.transform = `rotate(${hour}deg)`;
-document.querySelector('.minute').style.transform = `rotate(${minute}deg)`;
-document.querySelector('.seconde').style.transform = `rotate(${second}deg)`;
+document.querySelector('.heure').style.transform = `rotateZ(${hour}deg)`;
+document.querySelector('.minute').style.transform = `rotateZ(${minute}deg)`;
+document.querySelector('.seconde').style.transform = `rotateZ(${second}deg)`;
 
-
+requestAnimationFrame(clock)
 }
 
-setInterval(clock, 1000);
